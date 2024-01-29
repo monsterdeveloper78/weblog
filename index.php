40bb9b10230e3b23fe8ff8d7c558201d2556f01d
Amir Kashir <masterdeveloper78@gmail.com>
2024-01-29 17:49:52 +0330

    128

--- index.php
+++ index.php
@@ -1,11 +1,97 @@
 <?php
-//start session
+
+//session start
 session_start();
+
+
 //config
 define('BASE_PATH', __DIR__);
-define('CURRENT_DOMAIN', currentDomain().' /weblog/' );
+define('CURRENT_DOMAIN', currentDomain() . '/weblog');
 define('DISPLAY_ERROR', true);
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'project');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
+
+
+//helpers
+
+function protocol() {
+    return  stripos($_SERVER['SERVER_PROTOCOL'], 'https') === true ? 'https://' : 'http://';
+}
+
+
+function currentDomain()
+{
+    return protocol() . $_SERVER['HTTP_HOST'];
+}
+
+
+function asset($src){
+
+    $domain = trim(CURRENT_DOMAIN, '/ ');
+    $src = $domain . '/' . trim($src, '/');
+    return $src;
+}
+
+function url($url){
+
+    $domain = trim(CURRENT_DOMAIN, '/ ');
+    $url = $domain . '/' . trim($url, '/');
+    return $url;
+}
+
+function currentUrl()
+{
+    return currentDomain() . $_SERVER['REQUEST_URI'];
+}
+
+function methodField()
+{
+    return $_SERVER['REQUEST_METHOD'];
+}
+
+function displayError($displayError)
+{
+
+    if($displayError)
+    {
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
+    }
+    else
+    {
+        ini_set('display_errors', 0);
+        ini_set('display_startup_errors', 0);
+        error_reporting(0);
+    }
+
+}
+
+displayError(DISPLAY_ERROR);
+
+
+global $flashMessage;
+if(isset($_SESSION['flash_message'])){
+    $flashMessage = $_SESSION['flash_message'];
+    unset($_SESSION['flash_message']);
+}
+
+
+function flash($name, $value = null)
+{
+    if($value === null){
+        global $flashMessage;
+        $message = isset($flashMessage[$name]) ? $flashMessage[$name] : '';
+        return $message;
+    }
+    else{
+        $_SESSION['flash_message'][$name] = $value;
+    }
+
+}
+ flash('login_error', 'ورود با خطا مواجه شد');
+ flash('cart_success', 'محصول با موفقیت به سبد خرید شما اضافه شد');
+ echo flash('login_error');
+ echo flash('cart_success');
